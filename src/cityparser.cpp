0c8ae9832b40d553e6fab2e6845c3f47eb22e938
Howard-12 <s0989175085@gmail.com>
2024-08-17 17:29:07 +1000

    set root dir evn var

--- src/cityparser.cpp
+++ src/cityparser.cpp
@@ -1,59 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 
 #include "cityparser.hpp"
 
+#define PROJECT_DIR std::string(PROJECT_SOURCE_DIR)
 
 std::vector<CityLocation> parseCSVFile(std::string filename) {
-    std::ifstream input(filename);
-
+    std::string path = std::string(PROJECT_DIR) + filename;
+    std::ifstream input(path.c_str());
     if (!input.is_open()) {
-    std::cerr << "Couldn't read file: " << filename << "\n";
-        return {};
+      std::cerr << "Couldn't read file: " << filename << "\n";
+      return {};
     }
 
     std::vector<CityLocation> cities;
     std::vector<std::string> headRow;
     
-    
-    std::string cr, cx, cy, chs, cloot;
-    input >> cr >> cx >> cy >> chs >> cloot;
+    for (std::string line; std::getline(input, line);) {
+        std::istringstream ss(std::move(line));
+        std::vector<std::string> row;
 
-    std::cout << "Read: ";
-    std::cout << cr << " " << cx << " " << cy << " " << chs << " " << cloot << "\n";
+        for (std::string value; std::getline(ss, value, ',');) {
+            row.push_back(std::move(value));
+        }
 
-    /*for (int i == 0; )*/
-    /*CityLocation newCity;*/
-    /*newCity.x = -1200;*/
-    /*newCity.y = 4600;*/
-    /*newCity.hasShip = 0;*/
-    /*newCity.looted = 0;*/
-    /**/
-    /*cities.push_back(newCity);*/
-    /**/
-    /*for (std::string line; std::getline(input, line);) {*/
-    /*    std::istringstream ss(std::move(line));*/
-    /*    std::vector<std::string> row;*/
-    /**/
-    /*    for (std::string value; std::getline(ss, value, ',');) {*/
-    /*        row.push_back(std::move(value));*/
-    /*    }*/
-    /**/
-    /*    if (headRow.empty()) {*/
-    /*        // Skip this row*/
-    /*        headRow = row;*/
-    /*        continue;*/
-    /*    }*/
-    /**/
-    /*    CityLocation newCity;*/
-    /*    newCity.x = std::stoi(row.at(0));*/
-    /*    newCity.y = std::stoi(row.at(1));*/
-    /*    newCity.hasShip = row.at(2) == "1";*/
-    /*    newCity.looted = row.at(3) == "1";*/
-    /**/
-    /*    cities.push_back(newCity);*/
-    /*}*/
+        if (headRow.empty()) {
+            // Skip this row
+            headRow = row;
+            continue;
+        }
+
+        CityLocation newCity;
+        newCity.x = std::stoi(row.at(0));
+        newCity.y = std::stoi(row.at(1));
+        newCity.hasShip = row.at(2) == "1";
+        newCity.looted = row.at(3) == "1";
+
+        cities.push_back(newCity);
+    }
     return cities;
 }
 
@@ -68,9 +54,9 @@ std::vector<CityLocation> readCitiesAround(uint64_t seed, int x, int z) {
 
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            std::cout << "Loading chunk " << i << " " << j << "\n";
-            filename << "../searched/" << seed << "." << r*(rx+i) << "." << r*(rz+j) << ".csv";
-            std::cout << filename.str() << "\n";
+            /*std::cout << "Loading chunk " << i << " " << j << "\n";*/
+            filename << "searched/" << seed << "." << r*(rx+i) << "." << r*(rz+j) << ".csv";
+            /*std::cout << filename.str() << "\n";*/
             child = parseCSVFile(filename.str());
             mommy.insert(mommy.end(), child.begin(), child.end());
             filename.str(std::string());
