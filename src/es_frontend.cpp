8951f27410769f84ee4e0de58edfe394129ec895
MiiKaa3 <m.kasumagic@gmail.com>
2024-08-17 11:10:51 +1000

    lets fix this

---
+++ src/es_frontend.cpp
@@ -0,0 +1,46 @@
+#include "es_frontend.hpp"
+#include "generator.h"
+
+es::colorMap_t generate_ColorMap ()
+{
+    Generator g;
+    uint64_t seed = 1;
+    setupGenerator(&g, MC_1_20, 0);
+    applySeed(&g, DIM_END, seed);
+    
+    es::colorMap_t CM { std::make_shared<std::array<std::array<sf::Color, 150>, 150>>() };
+    for (uint64_t x = 0; x < CM->size(); x++)
+    {
+        for (uint64_t z = 0; z < CM->at(x).size(); z++) 
+        {
+            int biomeID = getBiomeAt(&g, 1, -1800+(x*8), 63, 4000+(z*8)); // scale, x, y, z 
+            if (biomeID == small_end_islands)    
+                CM->at(x).at(z) = es::color_void;
+            else if (biomeID == end_barrens)
+                CM->at(x).at(z) = es::color_barr;
+            else if (biomeID == end_midlands)
+                CM->at(x).at(z) = es::color_midl;
+            else if (biomeID == end_highlands)
+                CM->at(x).at(z) = es::color_high;
+        }
+    }
+    return CM;
+}
+
+std::shared_ptr<sf::RenderTexture> generate_map (const sf::Window& win, const es::colorMap_t& CM)
+{
+      std::shared_ptr<sf::RenderTexture> map { std::make_shared<sf::RenderTexture>() };
+      map->create(win.getSize().x, win.getSize().y);
+      map->clear();
+      for (uint64_t x = 0; x < CM->size(); x++)
+      {
+          for (uint64_t z = 0; z < CM->at(x).size(); z++) 
+          {
+              sf::RectangleShape rect {{4.f, 4.f}};
+              rect.setPosition({static_cast<float>(x * 4), static_cast<float>(win.getSize().y - z * 4)});
+              rect.setFillColor(CM->at(x).at(z));
+              map->draw(rect);
+          }
+      }
+      return map;
+  }
--- src/main.cpp
+++ src/main.cpp
@@ -7,57 +7,8 @@
 #include <iostream>
 #include <array>
 
-namespace ES{
-    const sf::Color color_void { 0, 0, 42 };
-    const sf::Color color_barr { 90, 90, 72 };
-    const sf::Color color_midl { 235, 248, 182 };
-    const sf::Color color_high { 195, 189, 89 };
-    using colorMap_t = std::shared_ptr<std::array<std::array<sf::Color, 150>, 150>>;
+#include "es_frontend.hpp"
 
-    ES::colorMap_t generate_ColorMap ()
-    {
-        Generator g;
-        uint64_t seed = 1;
-        setupGenerator(&g, MC_1_20, 0);
-        applySeed(&g, DIM_END, seed);
-        
-        ES::colorMap_t CM { std::make_shared<std::array<std::array<sf::Color, 150>, 150>>() };
-        for (uint64_t x = 0; x < CM->size(); x++)
-        {
-            for (uint64_t z = 0; z < CM->at(x).size(); z++) 
-            {
-                int biomeID = getBiomeAt(&g, 1, -1800+(x*8), 63, 4000+(z*8)); // scale, x, y, z 
-                if (biomeID == small_end_islands)    
-                    CM->at(x).at(z) = ES::color_void;
-                else if (biomeID == end_barrens)
-                    CM->at(x).at(z) = ES::color_barr;
-                else if (biomeID == end_midlands)
-                    CM->at(x).at(z) = ES::color_midl;
-                else if (biomeID == end_highlands)
-                    CM->at(x).at(z) = ES::color_high;
-            }
-        }
-        return CM;
-    }
-
-    std::shared_ptr<sf::RenderTexture> generate_map (const sf::Window& win, const ES::colorMap_t& CM)
-    {
-        std::shared_ptr<sf::RenderTexture> map { std::make_shared<sf::RenderTexture>() };
-        map->create(win.getSize().x, win.getSize().y);
-        map->clear();
-        for (uint64_t x = 0; x < CM->size(); x++)
-        {
-            for (uint64_t z = 0; z < CM->at(x).size(); z++) 
-            {
-                sf::RectangleShape rect {{4.f, 4.f}};
-                rect.setPosition({static_cast<float>(x * 4), static_cast<float>(win.getSize().y - z * 4)});
-                rect.setFillColor(CM->at(x).at(z));
-                map->draw(rect);
-            }
-        }
-        return map;
-    }
-}
 
 int main()
 {
@@ -69,8 +20,8 @@ int main()
         return 1;
     }
 
-    ES::colorMap_t colorMap { ES::generate_ColorMap() }; 
-    std::shared_ptr<sf::RenderTexture> map { ES::generate_map(window, colorMap) };
+    es::colorMap_t colorMap { es::generate_ColorMap() }; 
+    std::shared_ptr<sf::RenderTexture> map { es::generate_map(window, colorMap) };
     sf::Sprite mapSprite { map->getTexture() };
     
     sf::Clock deltaClock;
