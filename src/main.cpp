b7083a6a249524e828eb3d7d2d23e662017dde85
Gideon McKinlay <gmckinlay04@gmail.com>
2024-08-17 12:19:37 +1000

    Plot some cities!

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "finders.h"
 #include <memory>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <array>
 
 namespace ES{
@@ -17,7 +19,7 @@ namespace ES{
     ES::colorMap_t generate_ColorMap ()
     {
         Generator g;
-        uint64_t seed = 1;
+        uint64_t seed = 2438515238773172647;
         setupGenerator(&g, MC_1_20, 0);
         applySeed(&g, DIM_END, seed);
         
@@ -26,7 +28,7 @@ namespace ES{
         {
             for (uint64_t z = 0; z < CM->at(x).size(); z++) 
             {
-                int biomeID = getBiomeAt(&g, 1, -1800+(x*8), 63, 4000+(z*8)); // scale, x, y, z 
+                int biomeID = getBiomeAt(&g, 1, -2500+(x*16), 63, 4000+(z*16)); // scale, x, y, z 
                 if (biomeID == small_end_islands)    
                     CM->at(x).at(z) = ES::color_void;
                 else if (biomeID == end_barrens)
@@ -59,8 +61,71 @@ namespace ES{
     }
 }
 
-int main()
-{
+class CityLocation {
+public:
+    int x;
+    int y;
+    bool hasShip;
+    bool looted;
+};
+
+int main() {
+    std::string filename{"searched/2438515238773172647.-5000.0.csv"};
+    std::ifstream input{filename};
+
+    if (!input.is_open()) {
+    std::cerr << "Couldn't read file: " << filename << "\n";
+        return 1; 
+    }
+
+    std::vector<CityLocation> cities;
+
+    std::vector<std::string> headRow;
+
+    CityLocation newCity;
+    newCity.x = -1200;
+    newCity.y = 4600;
+    newCity.hasShip = 0;
+    newCity.looted = 0;
+
+    cities.push_back(newCity);
+
+    for (std::string line; std::getline(input, line);) {
+        std::istringstream ss(std::move(line));
+        std::vector<std::string> row;
+
+        // if (!cities.empty()) {
+        //     // We expect each row to be as big as the first row
+        //     row.reserve(cities.front().size());
+        // }
+        // std::getline can split on other characters, here we use ','
+        for (std::string value; std::getline(ss, value, ',');) {
+            row.push_back(std::move(value));
+        }
+
+        if (headRow.empty()) {
+            // Skip this row
+            headRow = row;
+            continue;
+        }
+
+        CityLocation newCity;
+        newCity.x = std::stoi(row.at(0));
+        newCity.y = std::stoi(row.at(1));
+        newCity.hasShip = row.at(2) == "1";
+        newCity.looted = row.at(3) == "1";
+
+        cities.push_back(newCity);
+    }
+
+    // Print out our table
+    for (const CityLocation& city : cities) {
+        std::cout << city.x << ", " << city.y << ", " << city.hasShip << ", " << city.looted;
+        std::cout << "\n";
+    }
+
+    printf("Hello World!\n");
+
     sf::RenderWindow window(sf::VideoMode(600, 600), "Elytra Sniper");
     window.setPosition({600, 20});
     window.setFramerateLimit(60);
@@ -87,6 +152,20 @@ int main()
         window.clear();
 
         window.draw(mapSprite);
+
+        for (const CityLocation& city : cities) {
+            int mx = (city.x + 2500) / 16*4;
+            int my = (city.y - 4000) / 16*4;
+            sf::CircleShape circle;
+            circle.setRadius(10);
+            if (city.hasShip) {
+                circle.setFillColor(sf::Color::Green);
+            } else {
+                circle.setFillColor(sf::Color::Red);
+            }
+            circle.setPosition(mx, my);
+            window.draw(circle);
+        }
 
         ImGui::SFML::Update(window, deltaClock.restart());
         ImGui::ShowDemoWindow();
